3c9477e45e923b28d9677dc6291e35bb7c833c28
Kris Nuttycombe <kris@nutty.land>
2022-11-26 12:35:23 -0700

    Add flake.nix using updated dbmigrations.

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "PostgreSQL (via HDBC) runner for dbmigrations";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    dbmigrations.url = "github:nuttycom/dbmigrations/74ef9388b45ae73a1d9c737d9644e076fe832672";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, dbmigrations }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkg-name = "dbmigrations-postgresql";
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+          haskell = pkgs.haskellPackages;
+
+          haskell-overlay = final: prev: {
+            dbmigrations = dbmigrations.defaultPackage.${system};
+            ${pkg-name} = hspkgs.callCabal2nix pkg-name ./. {};
+          };
+
+          hspkgs = haskell.override {
+            overrides = haskell-overlay;
+          };
+      in {
+        packages = pkgs;
+
+        defaultPackage = hspkgs.${pkg-name};
+
+        devShell = hspkgs.shellFor {
+          packages = p: [p.${pkg-name}];
+          root = ./.;
+          withHoogle = true;
+          buildInputs = with hspkgs; [
+            haskell-language-server
+            cabal-install
+          ];
+        };
+      }
+    );
+}
